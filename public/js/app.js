804b3128a2b3c51784fb0d0091f729f025145375
Erin Trainor <trainor.erin@gmail.com>
2019-08-25 22:45:54 -0700

    added pastor and church add forms

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,13 @@
 // $('.select-button').on('click', function() {
 //   $(this).next().removeClass('hide-me');
 // });
+$('.add-church-form').hide();
+$('.add-pastor-form').hide();
 
-$('#add-church-button').on('click', function() {
-  $('#add-church-form').toggle();
+$('.add-church-button').on('click', function() {
+  $('.add-church-form').toggle();
 })
 
-$('#add-pastor-button').on('click', function() {
-  $('#add-pastor-form').toggle();
+$('.add-pastor-button').on('click', function() {
+  $('.add-pastor-form').toggle();
 })
