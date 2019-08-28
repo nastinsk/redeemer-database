2649dea257330e03e73e1bc0f19be1357c5ba9c0
Erin Trainor <trainor.erin@gmail.com>
2019-08-27 22:00:23 -0700

    fixed add button toggle

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,19 @@ $('.add-church-form').hide();
 $('.add-pastor-form').hide();
 
 $('.add-church-button').on('click', function () {
+  if($('.add-pastor-button').hasClass('selected') === true){
+    $('.add-pastor-form').hide();
+    $('.add-pastor-button').removeClass('selected');
+  }
   $('.add-church-form').toggle();
   $('.add-church-button').toggleClass('selected');
 })
 
 $('.add-pastor-button').on('click', function () {
+  if($('.add-church-button').hasClass('selected') === true){
+    $('.add-church-form').hide();
+    $('.add-church-button').removeClass('selected');
+  }
   $('.add-pastor-form').toggle();
   $('.add-pastor-button').toggleClass('selected');
 })
